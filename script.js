a483d16aebdc2d2cb1e8b5b5c41907d1c81b221b
kenykenoei <matheuschavier112004@gmail.com>
2021-06-02 16:18:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let age1 = 18
+if (age1 >= 18){
+    console.log("você é maior de idade")
+} else{
+    console.log("você é menor de idade")
+}
+let age2 = 18
+let iAmHumam = true
+if (age2 >= 18 && iAmHumam === true){
+    console.log("você é maior de idade e é humano")
+} else{
+    console.log("você não é maior de idade ou não é humano")
+}
+let birthMonth = "dezenbro"
+if (birthMonth === "janeiro", "dezenbro"){
+    console.log("você nasceu em janeiro ou dezenbro")
+} else{
+    console.log("você nasceu no meio do ano")
+}
